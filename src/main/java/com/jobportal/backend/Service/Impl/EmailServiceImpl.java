b00db30cd5188dc6c4f519ac01a47e1676854ea0
Quốc Nguyễn <anhquoc3241@gmail.com>
2026-01-19 23:39:13 +0700

    fix: api sendgrid

--- src/main/java/com/jobportal/backend/Service/Impl/EmailServiceImpl.java
+++ src/main/java/com/jobportal/backend/Service/Impl/EmailServiceImpl.java
@@ -1,37 +1,41 @@
 package com.jobportal.backend.Service.Impl;
 
 import com.jobportal.backend.Service.EmailService;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private final JavaMailSender mailSender;
+    private final String FROM_EMAIL = System.getenv("MAIL_FROM");  // verified email
+    private final String SENDGRID_API_KEY = System.getenv("SENDGRID_API_KEY");
 
     @Override
     public void sendEmail(String to, String subject, String content) {
+        Email from = new Email(FROM_EMAIL);
+        Email toEmail = new Email(to);
+        Content emailContent = new Content("text/plain", content);
+        Mail mail = new Mail(from, subject, toEmail, emailContent);
+
+        SendGrid sg = new SendGrid(SENDGRID_API_KEY);
+        Request request = new Request();
+
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(content);
-
-            // Lấy email gửi từ biến môi trường
-            String fromEmail = System.getenv("MAIL_FROM");
-            if (fromEmail == null || fromEmail.isEmpty()) {
-                fromEmail = "no-reply@jobportal.com"; // fallback
-            }
-            message.setFrom(fromEmail);
-
-            mailSender.send(message);
-            System.out.println("✔ Email sent to " + to);
-        } catch (Exception e) {
-            System.out.println("❌ Failed to send email to " + to + ": " + e.getMessage());
-            throw new RuntimeException("Cannot send email");
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println("Email sent! Status: " + response.getStatusCode());
+        } catch (IOException ex) {
+            System.out.println("❌ Failed to send email to " + to + ": " + ex.getMessage());
+            throw new RuntimeException("Cannot send email via SendGrid API");
         }
     }
 }
